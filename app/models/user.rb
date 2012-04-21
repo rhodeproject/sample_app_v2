2da9c9f8ce605668712b1c17fe0f830c057ee629
rhodeproject <mhatch73@gmail.com>
2012-04-20 23:08:51 -0400

    user model

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+
+  #convert email to Lowercase to ensure uniqueness in the DB
+  before_save { |user| user.email = email.downcase}
+
+  #user validation
+  validates :name, presence: true, length:{maximum: 50}
+
+  #email validation
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+
+  #password validation
+  validates :password, length: {minimum: 6}
+
+  #password_confirmation validation
+  validates :password_confirmation, presence: true
+
+end
