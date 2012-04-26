878516d64281b57b335e54b872e535cd1ddd2f6c
rhodeproject <mhatch73@gmail.com>
2012-04-25 23:03:51 -0400

    PostrgreSQL

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,20 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
-  has_many :microposts, dependent: :destroy
 
-  before_save { |user| user.email = email.downcase }
+  #Data Relationships
+  has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through:  :relationships, source: :followed
+
+  #reverse relationship
+  has_many :reverse_relationships, foreign_key: "followed_id",
+           class_name:  "Relationship",
+           dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
+  #repeated code -- commented 4/24 --
+  #before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
   #convert email to Lowercase to ensure uniqueness in the DB
@@ -35,8 +46,24 @@ class User < ActiveRecord::Base
   #password_confirmation validation
   validates :password_confirmation, presence: true
 
+  #scopes
+  scope :admin, where(admin: true)
+
+
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
